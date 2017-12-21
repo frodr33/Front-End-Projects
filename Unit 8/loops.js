d98c0d4ace6feea9243d55524329088a3c97fb41
Frank <fsr32@cornell.edu>
2017-12-21 11:50:13 -0500

    Adding some Javascript Loops Practice

---
+++ Unit 8/loops.js
@@ -0,0 +1,30 @@
+var startNum = -10;
+while (startNum <= 19) {
+	console.log(startNum);
+	startNum++;
+}
+
+
+var startNum1 = 10;
+while (startNum1 <= 40) {
+	if (startNum1 % 2 == 0) {
+		console.log(startNum1);
+	}
+	startNum1++;
+}	
+
+var startNum2 = 300;
+while (startNum2 <= 333){
+	if (startNum2 % 2 != 0) {
+		console.log(startNum2);
+	}
+	startNum2++;
+}
+
+var startNum3 = 5;
+while (startNum3 <= 50) {
+	if (startNum3 % 5 == 0 && startNum3 % 3 == 0) {
+		console.log(startNum3);
+	}
+	startNum3++;
+}
